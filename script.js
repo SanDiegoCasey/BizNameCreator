2829ebda3af6fb69c12f33af5d43c824a685755d
Casey Whitcher <sandiegocasey@gmail.com>
2018-06-05 14:45:32 -0700

    added namecheap keys and styling

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const NAMEURL = "https://uzby.com/api.php"
+const NAME = ""
+const NAMEIP = "107.201.225.22"
+const NAMECOMMAND = "namecheap.domains.check"
+const NAMECHEAPURL = "https://api.sandbox.namecheap.com/xml.response"
+const NAMECHEAPUSER = "SanDiegoCasey"
+const NAMECHEAPAPI = "518bfd94312a49c595938b11e78d1ee2"
+
+
+// SAMPLE STRING https://api.sandbox.namecheap.com/xml.response?ApiUser=SanDiegoCasey&ApiKey=518bfd94312a49c595938b11e78d1ee2&UserNAme=SanDiegoCasey&ClientIp=107.201.225.22&Command=namecheap.domains.check&DomainList=somthing3333.com,something.com
 
 // get data from api
 function getDataFromApi(value, callback){
+  console.log("testing");
   const QUERY = {
     min:`${value}`,
     max:`${value}`
   }
-  $.getJSON(NAMEURL, QUERY, callback)
+  $.getJSON(NAMEURL, QUERY, callback);
 }
 
 function renderResult(result){
