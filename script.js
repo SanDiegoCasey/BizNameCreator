7a8ec774461c2e9e80442feba63ba2eb2878cffd
Casey Whitcher <sandiegocasey@gmail.com>
2018-06-05 15:54:51 -0700

    pushing to github

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ const NAMECHEAPURL = "https://api.sandbox.namecheap.com/xml.response"
 const NAMECHEAPUSER = "SanDiegoCasey"
 const NAMECHEAPAPI = "518bfd94312a49c595938b11e78d1ee2"
 
-
 // SAMPLE STRING https://api.sandbox.namecheap.com/xml.response?ApiUser=SanDiegoCasey&ApiKey=518bfd94312a49c595938b11e78d1ee2&UserNAme=SanDiegoCasey&ClientIp=107.201.225.22&Command=namecheap.domains.check&DomainList=somthing3333.com,something.com
 
 // get data from api
 function getDataFromApi(value, callback){
-  console.log("testing");
   const QUERY = {
-    min:`${value}`,
-    max:`${value}`
+    min:value,
+    max:value
   }
   $.getJSON(NAMEURL, QUERY, callback);
 }
 
 function renderResult(result){
-  return `${result}`;
+  return result;
 }
 
 // render results to page
