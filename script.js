88c5d1f06370ae2d7fed028975fd86002a3500c9
Casey Whitcher <sandiegocasey@gmail.com>
2018-06-07 12:04:37 -0700

    added domain results

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-const NAMEURL = "https://cors-anywhere.herokuapp.com/https://uzby.com/api.php"
-const NAME = ""
-const NAMEIP = "107.201.225.22"
-const NAMECOMMAND = "namecheap.domains.check"
-const NAMECHEAPURL = "https://api.sandbox.namecheap.com/xml.response"
-const NAMECHEAPUSER = "SanDiegoCasey"
-const NAMECHEAPAPI = "518bfd94312a49c595938b11e78d1ee2"
-
-// SAMPLE STRING https://api.sandbox.namecheap.com/xml.response?ApiUser=SanDiegoCasey&ApiKey=518bfd94312a49c595938b11e78d1ee2&UserNAme=SanDiegoCasey&ClientIp=107.201.225.22&Command=namecheap.domains.check&DomainList=somthing3333.com,something.com
+const DOMAINRURLSTATUS = "https://domainr.p.mashape.com/v2/status"
+const DOMAINRKEY = "4QEWXh45IWmshVb3XvKWKF09djZkp15M5YXjsn0HlZtIxTnaB8"
 
 // get data from api
 function getDataFromApi(value, callback){
   const QUERY = {
-    min:value,
-    max:value
+    "mashape-key": DOMAINRKEY,
+    domain: value+".com,"+value+".net,"+value+".org,"+value+".biz"
   }
-  $.getJSON(NAMEURL, QUERY, callback);
+  $.getJSON(DOMAINRURLSTATUS, QUERY, callback);
 }
 
 function renderResult(result){
-  return result;
+  const OUTPUT = $('#domainResults');
+  OUTPUT
+      .prop('hidden', false);
+  if (result.summary == "inactive"){
+    return `<div class="domain">${result.domain}<span class="buyButton"><a href="https://www.namecheap.com/domains/registration/results.aspx?domain=${result.domain}" target="_blank">Buy it!</a></span></div>`;
+  }  else if (result.summary == "active"){
+    return `<div class="domainUnavail">${result.domain}<span class="sold">Unavailable</span></div>`;
+  } else {
+    return `<div class="domainUnavail">${result.domain}<span class="sold">Unavailable</span></div>`;
+  }
 }
 
 // render results to page
-function displayName(data){
-  console.log(displayName());
-  const results = renderResult(data);
-  $('.nameResult').html(results);
+function displayDomainResults(data){
+  const RESULTS = data.status.map((item, index) => renderResult(item));
+  $('#domainResults').html(RESULTS);
 }
 
-// wait for user to submit
-function watchSubmit() {
-  $('.js-search-form').submit(event => {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('#js-dropValue');
-    const thisquery = queryTarget.val();
-    getDataFromApi(thisquery, displayName);
-  });
+
+// script for name generation
+function get_new_name(){
+  const OUTPUT = $('#nameResult');
+  OUTPUT
+      .prop('hidden', false);
+      $('#nameResult').html(`<img src="ajax-loader.gif">`);
+  var xmlhttp;
+  if(window.XMLHttpRequest){
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  var
+  // get value from drop down
+    minlen = document.getElementById('js-dropValue').value,
+    maxlen = document.getElementById('js-dropValue').value,
+    param = 'min='+minlen+'&max='+maxlen;
+  // Get data from name generator
+  xmlhttp.open('GET', 'https://cors-anywhere.herokuapp.com/https://uzby.com/api.php?'+ param, true);
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      document.getElementById('nameResult').innerHTML = xmlhttp.responseText;
+    }
+    const THISQUERY = xmlhttp.responseText;
+    getDataFromApi(THISQUERY, displayDomainResults);
+  };
+  xmlhttp.send();
+  return false;
 }
-// running the watch submit function waiting for click
-$(watchSubmit);
